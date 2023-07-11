7f5d09f3f2d1185d823767e4b81583b4286acd72
thomasmso <5104410+thomasmso@users.noreply.github.com>
2023-07-10 18:08:27 -0700

    1.1.2

--- plugin/www/applovinmax.js
+++ plugin/www/applovinmax.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 var cordova = require('cordova');
 
-const VERSION = '1.1.1';
+const VERSION = '1.1.2';
 
 /**
  * This enum represents whether or not the consent dialog should be shown for this user.
